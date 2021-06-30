e171f17f562d12f25e7d9f8f2d817be8927317ef
escarolin <escarolin09@hotmail.com>
2021-06-30 18:33:54 -0400

    Modles terminada

---
+++ Models/Cobros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escarolin_P2_AP2.Models
+{
+    public class Cobros
+    {
+        [Key]
+        public int CobroId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Debe realizar un cobro")]
+        public double TotalCobrado { get; set; }
+        public string Observaciones { get; set; }
+
+        [ForeignKey("CobroId")]
+        public virtual List<CobrosDetalle> Detalle { get; set; } = new List<CobrosDetalle>();
+    }
+}
+ 
---
+++ Models/CobrosDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escarolin_P2_AP2.Models
+{
+    public class CobrosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CobroId { get; set; }
+        public Cobros Cobro { get; set; }
+        public int VentaId { get; set; }
+        public  Ventas Venta { get; set; }
+        public double Cobrado { get; set; }
+    }
+}
